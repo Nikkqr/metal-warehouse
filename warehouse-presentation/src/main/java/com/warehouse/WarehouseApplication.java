769b951ae7ffdf08d0501fd659a4c3098c3bd21b
Bushkov Nikita <408338@niuitmo.ru>
2026-04-23 22:07:09 +0300

    docker-compose added

--- warehouse-presentation/src/main/java/com/warehouse/WarehouseApplication.java
+++ warehouse-presentation/src/main/java/com/warehouse/WarehouseApplication.java
@@ -2,8 +2,10 @@ package com.warehouse;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.persistence.autoconfigure.EntityScan;;
 
 @SpringBootApplication
+@EntityScan(basePackages = {"com.warehouse.entities"})
 public class WarehouseApplication {
     public static void main(String[] args) {
         SpringApplication.run(WarehouseApplication.class, args);
